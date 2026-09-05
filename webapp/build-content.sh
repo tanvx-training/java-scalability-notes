f71d50cf26629ea33111f45ebceea179aa1d2fa4
tanvx <tanvx.training@gmail.com>
2026-09-05 21:31:23 +0700

    chore: chuẩn hoá nguồn CKA Study Guide thành cka-book-vi/ (23 tệp, 50 ảnh) và nối vào build-content

--- webapp/build-content.sh
+++ webapp/build-content.sh
@@ -14,7 +14,7 @@ DEST="$(cd "$DEST" && pwd)"
 mkdir -p "$DEST/java" "$DEST/images" "$DEST/sysprog/images" \
          "$DEST/k8sbook/images" "$DEST/springsec" "$DEST/senior" \
          "$DEST/modconc/images" "$DEST/ddia/images" "$DEST/mjia/images" \
-         "$DEST/kafka/images" "$DEST/springstart/images"
+         "$DEST/kafka/images" "$DEST/springstart/images" "$DEST/ckabook/images"
 
 cp "$REPO"/CKAD/*.md "$REPO"/CKA/*.md "$REPO"/CKS/*.md  "$DEST/"
 cp "$REPO/Chủ đề"*/*.md                                 "$DEST/java/"
@@ -35,3 +35,5 @@ cp    "$REPO"/kafka-vi/*.md                              "$DEST/kafka/"
 cp -R "$REPO"/kafka-vi/images/.                          "$DEST/kafka/images/"
 cp    "$REPO"/spring-start-vi/*.md                       "$DEST/springstart/"
 cp -R "$REPO"/spring-start-vi/images/.                   "$DEST/springstart/images/"
+cp    "$REPO"/cka-book-vi/*.md                           "$DEST/ckabook/"
+cp -R "$REPO"/cka-book-vi/images/.                       "$DEST/ckabook/images/"
